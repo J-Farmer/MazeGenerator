ad70653cc764cd859c136bdd2a435f65c8ea8619
Jake Farmer <jfarmer0495@gmail.com>
2020-04-09 15:43:16 -0500

    Combined DistanceGrid and Grid, Added dijkstra.py

--- Grid.py
+++ Grid.py
@@ -166,3 +166,29 @@ class Grid():
         
         im.save(name)           
                 
+class DistanceGrid(Grid):
+    
+    def __init__(self, row, col):
+        self.distances = None
+        
+        super().__init__(row, col)
+    
+    def contentsOf(self, cell):
+        if self.distances == None:
+            return super().contentsOf(cell)
+        
+        return self.toBase36(self.distances[cell])
+        
+    def toBase36(self,num):
+        
+        alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+        
+        if(type(num) is not int):
+            raise TypeError("Number must be an integer!")
+        if(num < 0):
+            raise ValueError("Number must be positve!")
+            
+        idx = num % len(alphabet)
+        
+        return alphabet[idx]
+    
---
+++ dijkstra.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 15:37:45 2020
+
+@author: jfarm
+"""
+
+
+from BinaryTree import BinaryTree
+from Grid import DistanceGrid
+
+grid = DistanceGrid(5,5)
+
+BinaryTree.on(grid)
+start = grid[0][0]
+
+distances = start.distances()
+
+grid.distances = distances
+
+print(grid)
